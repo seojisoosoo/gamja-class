e2a082aefecbbbc0af35f1d192ccfe36050c5c27
Seojisoo20191941 <amy0480@naver.com>
2023-05-15 02:58:23 +0900

    [ feat ] 제목 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { ThemeProvider, styled } from "styled-components";
 import { GlobalStyle } from "./style/globalStyle";
 import { theme } from "./style/theme";
+import CardsPage from "./@pages/cardsPage";
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Title>웰컴~</Title>
+        <CardsPage />
       </ThemeProvider>
     </>
   );
 }
-
-const Title = styled.h1`
-  ${({ theme }) => theme.fonts.title}
-`;
