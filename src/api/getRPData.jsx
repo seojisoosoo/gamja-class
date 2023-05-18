5e9289551681d188c3a46181db76ae6ca9590557
Seojisoo20191941 <amy0480@naver.com>
2023-05-18 11:09:15 +0900

    [ feat ] api 연결 중

--- src/api/getRPData.jsx
+++ src/api/getRPData.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export async function getRPData() {
-  const data = await axios.get(`https://67f09d03-afd1-405f-bac1-4b16e02d1726.mock.pstmn.io/list/`);
+  const data = await axios.get(`http://localhost:3000/get`);
+
+  console.log(data.data);
+  // const data = await axios.get(`https://67f09d03-afd1-405f-bac1-4b16e02d1726.mock.pstmn.io/list/`);
   return data.data;
 }
