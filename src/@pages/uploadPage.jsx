58960e6d4eb11c66e6e2240c3c13b91f5868a626
Seojisoo20191941 <amy0480@naver.com>
2023-05-18 11:28:57 +0900

    [ feat ] onchange로 값 받아오기

--- src/@pages/uploadPage.jsx
+++ src/@pages/uploadPage.jsx
@@ -7,12 +7,22 @@ export default function UploadPage() {
   const nameRef = useRef(null);
   const imgRef = useRef(null);
 
+  function getName(e) {
+    setNewData((prev) => ({ ...prev, name: e.target.value }));
+  }
+
+  function getImg(e) {
+    setNewData((prev) => ({ ...prev, img: e.target.value }));
+  }
+
+  console.log(newData);
+
   return (
     <UploadPageWrapper>
       <p>이름</p>
-      <input type="text" ref={nameRef} />
+      <input type="text" ref={nameRef} onChange={getName} />
       <p>이미지</p>
-      <input type="text" ref={imgRef} />
+      <input type="text" ref={imgRef} onChange={getImg} />
       <Button type="button">확인</Button>
     </UploadPageWrapper>
   );
